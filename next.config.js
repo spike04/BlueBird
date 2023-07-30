249b8c98129bc2ba3fae9f205f2cc1e0ca905e8c
spike04 <rubinbajracharya@yahoo.com>
2023-07-31 00:03:55 +0545

    deploy app

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  experimental: { serverActions: true },
+  images: {
+    remotePatterns: [{ hostname: 'avatars.githubusercontent.com' }],
+  },
+}
 
 module.exports = nextConfig
